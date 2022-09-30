5ea4f964a503636e046bc99d36d548399cd75bc7
RalucaZota <zotaraluca649@gmail.com>
2022-09-30 11:32:58 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+//  "https://forkify-api.herokuapp.com/api/v2/recipes/";
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "f019acb2-b330-4520-ba91-4411daefaedf";
+export const MODAL_CLOSE_SEC = 2.5;
